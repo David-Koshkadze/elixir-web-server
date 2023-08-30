095adfa9137df0ece63c9e398721ace2745ec270
David-Koshkadze <rootUS3rr@pm.me>
2023-08-30 22:53:54 +0400

    initial commit w/cowboy

---
+++ cowboy_server/lib/cowboy_server.ex
@@ -0,0 +1,18 @@
+defmodule CowboyServer do
+  @moduledoc """
+  Documentation for `CowboyServer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CowboyServer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
