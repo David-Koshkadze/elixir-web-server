58c1746f6e64c7fca4f6d5195e59beeed852a5ba
David-Koshkadze <rootUS3rr@pm.me>
2023-08-31 12:34:37 +0400

    static html and HTTP method validation

--- cowboy_server/lib/cowboy_server/router.ex
+++ cowboy_server/lib/cowboy_server/router.ex
@@ -1,14 +1,16 @@
 defmodule CowboyServer.Router do
   require Logger
 
-  alias CowboyServer.Router.Handlers.{Root, Greet}
+  alias CowboyServer.Router.Handlers.{Root, Greet, Static}
 
   def routes do
     [
-      {:_, [
-        {"/", Root, []},
-        {"/greet/:who", [who: :nonempty], Greet, []}
-      ]}
+      {:_,
+       [
+         {"/", Root, []},
+         {"/greet/:who", [who: :nonempty], Greet, []},
+         {"/static/:page", [page: :nonempty], Static, []}
+       ]}
     ]
   end
 end
--- cowboy_server/lib/cowboy_server/router/handlers/root.ex
+++ cowboy_server/lib/cowboy_server/router/handlers/root.ex
@@ -1,7 +1,7 @@
 defmodule CowboyServer.Router.Handlers.Root do
   require Logger
 
-  def init(req0, state) do
+  def init(%{method: "GET"} = req0, state) do
     Logger.info("Received request: #{inspect(req0)}")
 
     req1 =
@@ -9,6 +9,23 @@ defmodule CowboyServer.Router.Handlers.Root do
         200,
         %{"content-type" => "text/html"},
         "Hello World",
+        req0
+      )
+
+    {:ok, req1, state}
+  end
+
+  @doc """
+  General clause for 404 error
+  """
+  def init(req0, state) do
+    Logger.info("Received request: #{inspect(req0)}")
+
+    req1 =
+      :cowboy_req.reply(
+        404,
+        %{"content-type" => "text/html"},
+        "404 Not Found",
         req0
       )
 
