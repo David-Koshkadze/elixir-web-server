6c64df513ecbe1b931e6fda57bc4f0e6fcb47745
David-Koshkadze <rootUS3rr@pm.me>
2023-08-31 11:52:17 +0400

    parse_qs query parameter

--- cowboy_server/lib/cowboy_server/router/handlers/greet.ex
+++ cowboy_server/lib/cowboy_server/router/handlers/greet.ex
@@ -6,11 +6,21 @@ defmodule CowboyServer.Router.Handlers.Greet do
 
     who = :cowboy_req.binding(:who, req0)
 
+    # custom query parameter
+
+    # curl http://localhost:5050/greet/Elixir\?greeting=Gamarjoba
+
+    greeting =
+      req0
+      |> :cowboy_req.parse_qs()
+      |> Enum.into(%{})
+      |> Map.get("greeting", @default_greeting)
+
     req1 =
       :cowboy_req.reply(
         200,
         %{"content-type" => "text/html"},
-        "Hello #{who}",
+        "#{greeting} #{who}",
         req0
       )
 
