0a64e2e53c07f6f73a1d32c20cb5bcacdfe15a4e
David-Koshkadze <rootUS3rr@pm.me>
2023-08-31 11:18:56 +0400

    router init/2

--- cowboy_server/lib/cowboy_server/router.ex
+++ cowboy_server/lib/cowboy_server/router.ex
@@ -1,5 +1,21 @@
 defmodule CowboyServer.Router do
+  require Logger
+
   def routes do
     {:_, [{"/", __Module__, []}]}
+  end
+
+  def init(req0, state) do
+    Logger.info("Received request: #{inspect(req0)}")
+
+    req1 =
+      :cowboy_req.reply(
+        200,
+        %{"content-type" => "text/html"},
+        "Hello World",
+        req0
+      )
+
+    {:ok, req1, state}
   end
 end
